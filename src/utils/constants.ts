6fcc998c011d7d9a22e76d9260cfe3fd35b5b3f7
KOSASIH <kosasihg88@gmail.com>
2024-08-29 13:02:25 +0700

    Create constants.ts

---
+++ src/utils/constants.ts
@@ -0,0 +1,14 @@
+export const API_ENDPOINTS = {
+  GAMES: '/games',
+  GAME_BY_ID: '/games/:id',
+  USERS: '/users',
+  USER_BY_ID: '/users/:id',
+};
+
+export const LOCAL_STORAGE_KEYS = {
+  TOKEN: 'token',
+  USER_ID: 'userId',
+};
+
+export const ROLE_ADMIN = 'admin';
+export const ROLE_USER = 'user';
