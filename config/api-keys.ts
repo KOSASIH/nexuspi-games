b213e664a896e59495540ea76a5a7215cc92aba3
KOSASIH <kosasihg88@gmail.com>
2024-08-29 12:04:48 +0700

    Create api-keys.ts

---
+++ config/api-keys.ts
@@ -0,0 +1,11 @@
+import { ApiKeys } from './types';
+
+const apiKeys: ApiKeys = {
+  // Pi Network API key
+  piNetworkApiKey: 'your_pi_network_api_key',
+
+  // Game token API key
+  gameTokenApiKey: 'your_game_token_api_key',
+};
+
+export default apiKeys;
