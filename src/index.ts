8f4a798900c5ced93002effd515db799936ad601
KOSASIH <kosasihg88@gmail.com>
2024-08-29 13:07:39 +0700

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import App from './App';
+import gameReducer from './reducers/gameReducer';
+import userReducer from './reducers/userReducer';
+
+const rootReducer = combineReducers({
+  game: gameReducer,
+  user: userReducer,
+});
+
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
